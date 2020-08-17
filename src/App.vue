44c49f071c88407a6664d6105de9dbf108cde2b6
businessboy <45738826+NikolaySimakov@users.noreply.github.com>
2020-08-18 01:51:26 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -29,17 +29,4 @@ label:focus,
   -webkit-appearance: none;
   box-shadow: none !important;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
