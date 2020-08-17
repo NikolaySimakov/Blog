9b40263f095dcec5b3594b013da38d0876076e4e
businessboy <45738826+NikolaySimakov@users.noreply.github.com>
2020-08-18 01:42:37 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as fb from '@/firebase'
-import router from '@/router/index'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    userProfile: {},
     posts: [],
     recentlySearched: []
   },
@@ -17,9 +15,6 @@ const store = new Vuex.Store({
     }
   },
   mutations: {
-    setUserProfile(state, profile) {
-      state.userProfile = profile
-    },
     setRecentlySearched(state, user) {
       let notInArray = (obj, list) => {
         let x;
@@ -51,47 +46,6 @@ const store = new Vuex.Store({
     }
   },
   actions: {
-    async signin({
-      dispatch
-    }, form) {
-      const {
-        user
-      } = await fb.auth.signInWithEmailAndPassword(form.email, form.password)
-      dispatch('fetchUserProfile', user)
-    },
-    async signup({
-      dispatch
-    }, form) {
-      let errorMessage = ''
-      await fb.auth.createUserWithEmailAndPassword(form.email, form.password)
-        .then(snapshot => {
-          const data = {
-            name: form.name,
-            email: form.email,
-            imageURL: form.imageURL
-          }
-          fb.users.doc(snapshot.user.uid).set(data)
-          dispatch('fetchUserProfile', snapshot.user)
-        })
-        .catch(error => {
-          errorMessage = error.message
-        })
-      return errorMessage
-    },
-    async fetchUserProfile({
-      commit
-    }, user) {
-      const userProfile = await fb.users.doc(user.uid).get()
-      commit('setUserProfile', userProfile.data())
-      if (['/signup', '/signin'].includes(router.currentRoute.path)) router.push('/')
-    },
-    async logout({
-      commit
-    }) {
-      await fb.auth.signOut()
-      commit('setUserProfile', {})
-      router.push('/signin')
-    },
     async recentlySearchedUsers({
       commit
     }, userData) {
