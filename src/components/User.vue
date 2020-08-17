afddeeb9b9b14b5c118dca2c5f82092e6bd3762c
businessboy <45738826+NikolaySimakov@users.noreply.github.com>
2020-08-17 07:04:44 +0300

    Removed unnecessary methods

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,64 +1,47 @@
 <template>
   <div>
-    <UserData :name="name" :email="email" :imageURL="imageURL"/>
-    <div>
-      <b-button
-        v-b-modal.add-post-modal
-        v-if="userPage()"
-        class="mt-3 mb-5"
-        variant="primary"
-      >+ add post</b-button>
-      <div class="d-flex justify-content-center mb-3 m-5" v-if="newPostLoading">
-        <b-spinner label="Loading..." variant="primary"></b-spinner>
-      </div>
-
-      <b-modal
-        id="add-post-modal"
-        ref="modal"
-        title="Make a post"
-        centered
-        @show="resetModal"
-        @ok="handleOk"
-      >
-        <b-form ref="form" @submit.stop.prevent="handleSubmit">
-          <b-form-group
-            :state="post.titleState"
-            label="Title"
-            label-for="title-input"
-            invalid-feedback="Title is required"
-          >
-            <b-form-input id="title-input" v-model="post.title" :state="post.titleState" required></b-form-input>
-          </b-form-group>
-          <b-form-group
-            :state="post.bodyState"
-            label="Post body"
-            label-for="body-textarea"
-            invalid-feedback="Post body is required"
-          >
-            <b-form-textarea
-                id="body-textarea"
-                v-model="post.body"
-                :state="post.bodyState"
-                rows="3"
-                max-rows="6"
-                required
-              ></b-form-textarea>
-          </b-form-group>
-          <b-button
-            @click="$refs.fileInput2.$el.querySelector('input[type=file]').click()"
-            variant="primary"
-          >Select post image</b-button>
-          <b-form-file v-model="post.image" style="display:none;" ref="fileInput2" />
-          <div class="mt-3">Selected image: {{ post.image ? post.image.name : '' }}</div>
-        </b-form>
-      </b-modal>
+    <UserData :name="name" :email="email" :imageURL="imageURL" />
+    <b-button
+      v-b-modal.add-post-modal
+      v-if="userPage()"
+      class="mt-3 mb-5"
+      variant="primary"
+    >+ add post</b-button>
+    <div class="d-flex justify-content-center mb-3 m-5" v-if="newPostLoading">
+      <b-spinner label="Loading..." variant="primary"></b-spinner>
     </div>
+
+    <b-modal
+      id="add-post-modal"
+      ref="modal"
+      title="Make a post"
+      centered
+      @show="resetModal"
+      @ok="handleOk"
+    >
+      <b-form-group label="Title" label-for="title-input" invalid-feedback="Title is required">
+        <b-form-input id="title-input" v-model="post.title" required></b-form-input>
+      </b-form-group>
+      <b-form-group
+        label="Post body"
+        label-for="body-textarea"
+        invalid-feedback="Post body is required"
+      >
+        <b-form-textarea id="body-textarea" v-model="post.body" rows="3" max-rows="6" required></b-form-textarea>
+      </b-form-group>
+      <b-button
+        @click="$refs.fileInput2.$el.querySelector('input[type=file]').click()"
+        variant="primary"
+      >Select post image</b-button>
+      <b-form-file v-model="post.image" style="display:none;" ref="fileInput2" />
+      <div class="mt-3">Selected image: {{ post.image ? post.image.name : '' }}</div>
+    </b-modal>
   </div>
 </template>
 
 <script>
 import * as fb from "@/firebase";
-import UserData from "./UserData.vue"
+import UserData from "./UserData.vue";
 export default {
   name: "User",
   components: {
@@ -69,9 +52,7 @@ export default {
       post: {
         image: null,
         title: "",
-        body: "",
-        titleState: null,
-        bodyState: null
+        body: ""
       },
       newPostLoading: false
     };
@@ -85,33 +66,16 @@ export default {
     userPage() {
       return fb.auth.currentUser.uid === this.$route.params.id;
     },
-    checkFormValidity() {
-      const valid = this.$refs.form.checkValidity();
-      this.post.titleState = valid;
-      this.post.bodyState = valid;
-      return valid;
-    },
     resetModal() {
       this.post = {
         image: null,
         title: "",
-        body: "",
-        titleState: null,
-        bodyState: null
+        body: ""
       };
     },
     async handleOk(bvModalEvt) {
       await this.addPost();
       bvModalEvt.preventDefault();
-      this.handleSubmit();
-    },
-    handleSubmit() {
-      if (!this.checkFormValidity()) {
-        return;
-      }
-      this.$nextTick(() => {
-        this.$bvModal.hide("add-post-modal");
-      });
     },
     addPost() {
       const img = this.post.image;
